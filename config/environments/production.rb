1a737c776c449753699c5c2a8e549f1ef7d12f6f
Tom Counsell <tom@counsell.org>
2011-07-22 06:24:40 +0100

    Updated the README and associated configuration for herok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,14 +38,14 @@ Soks::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => "2050-wiki.greenonblack.com" }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => 'gmail.com',
-    :user_name            => 'tom.counsell.decc.wiki@gmail.com',
-    :password             => 'xxxx',
-    :authentication       => 'plain',
-    :enable_starttls_auto => true  }
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
 
   # Enable threaded mode
   # config.threadsafe!
--- lib/tasks/send_change_notifications.rb
+++ lib/tasks/send_change_notifications.rb
@@ -1,1 +1,4 @@
-ChangeNotifications.send_all_change_notifications
+desc "This task is called by the Heroku cron add-on"
+task :cron => :environment do
+  ChangeNotifications.send_all_change_notifications
+end
