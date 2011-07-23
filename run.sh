df3dbb93137b954fc71381d9fdb59595512d69ef
Tom Counsell <tom@counsell.org>
2011-07-23 21:04:20 +0100

    Removed redundant files

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo "Deprecated"
-echo "Use the foreman gem"
-echo "foreman start --env config/development.env"
-echo "or"
-echo "foreman start --env config/production.env"
-echo "Should also have a crontab that triggers emails:  bundle exec rails runner 'ChangeNotifications.send_all_change_notifications'"
