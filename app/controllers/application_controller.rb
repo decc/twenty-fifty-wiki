a40d5e50bfa4474af743021f96add0b75775af65
Tom Counsell <tom@counsell.org>
2011-11-22 20:33:41 +0000

    Removed old debug messages from application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,7 @@ class ApplicationController < ActionController::Base
   before_filter :dump_format
   
   def dump_format
-    logger.info request.formats.inspect
-    logger.info request.accepts.inspect
     request.format ||= :html
-    logger.info request.accepts.inspect
-    logger.info request.formats.inspect
     true
   end
 
