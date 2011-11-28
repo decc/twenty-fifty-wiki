528364386c5340e7fc4ddde109d44b83168a4f15
Tom Counsell <tom@counsell.org>
2011-11-28 18:21:06 +0000

    Updated exception notification address

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use ExceptionNotifier,
   :email_prefix => "[DECC-WIKI] ",
-  :sender_address => %{"DECC WIKI" <joseph.downie@decc.gsi.gov.uk.decc.wiki@gmail.com>},
+  :sender_address => 'decc.wiki@gmail.com',
   :exception_recipients => %w{tom@counsell.org tom.counsell@decc.gsi.gov.uk}
