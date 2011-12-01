2952047b0387b7c9573b89a7a0e0d4b481eaaf66
Tom Counsell <tom@counsell.org>
2011-12-01 10:59:06 +0000

    Remove password protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  # before_filter :authenticate_user!, :except => [:waiting_for_confirmation,:show,:index,:home,:recent,:changes]
-  before_filter :authenticate_user!, :except => [:waiting_for_confirmation]
+  before_filter :authenticate_user!, :except => [:waiting_for_confirmation,:show,:index,:home,:recent,:changes]
+  # before_filter :authenticate_user!, :except => [:waiting_for_confirmation]
   before_filter :dump_format
   
   def dump_format
